7cb9f511cff213d772d7e28e5b5df653a1702f82
congtuong04 <congtuong0411@gmail.com>
2026-03-10 22:11:06 +0700

    fix mysql ssl connection

--- student-service/db.js
+++ student-service/db.js
@@ -9,13 +9,18 @@ function connectDB(){
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
+
  });
 
  db.connect((err)=>{
 
   if(err){
-   console.log("Waiting for MySQL...");
+   console.log("Waiting for MySQL...", err.message);
    setTimeout(connectDB,3000);
    return;
   }
